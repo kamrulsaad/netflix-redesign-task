67cfb1baf82458fd43393a5f71e1125b5960f4be
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-10-10 23:52:38 +0600

    added routing correction

--- src/Components/Thumbnail.js
+++ src/Components/Thumbnail.js
@@ -6,7 +6,7 @@ const Thumbnail = ({ movie }) => {
     const navigate = useNavigate()
 
     return (
-        <div onClick={() => navigate(`movie/${movie.id}`)}
+        <div onClick={() => navigate(`/movie/${movie.id}`)}
             className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}>
             <img
                 src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
